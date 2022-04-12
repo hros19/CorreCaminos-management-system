e48f40401439f8f6fbfdf30dae9f1e5cc25709dc
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-12 17:37:26 -0500

    New queries for maintenance logs

--- src/query/vehicle.query.js
+++ src/query/vehicle.query.js
@@ -9,6 +9,11 @@ const VEHICLE_QUERY = {
   UPDATE_VEHICLE: 'CALL upd_vehicle_details(?, ?, ?, ?, ?)',
   DELETE_VEHICLE: 'DELETE FROM Vehicle WHERE vehicle_id = ?',
   REGISTER_KILOMETERS: 'CALL reg_kilometers(?, ?)',
+  REGISTER_MAINTENANCELOG: 'CALL create_maint_log(?, ?)', //set
+  UPDATE_MAINTENANCELOG: 'CALL upd_maint_log(?, ?)',
+  GETP_MAINTENANCELOGS_OF_VEHICLE: 'CALL getp_veh_maint_logs(?, ?, ?)', // sets
+  GET_MAINTENANCELOGS_OF_VEHICLE: 'CALL get_veh_maint_logs(?)',
+  SELECT_MAINTENANCELOG: 'SELECT * FROM MaintenanceLog WHERE maintenance_id = ?'
 };
 
 export default VEHICLE_QUERY;
