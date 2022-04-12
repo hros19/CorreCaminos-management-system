3779f2a6160d920a60be2c3521e1ea0a61dfec30
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-11 22:34:33 -0600

    Create vehicle.route.js

---
+++ src/route/vehicle.route.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import { checkVehicleStatus, fillVehicleTank, createVehicle, getPagedVehicles, getVehicle, updateVehicle, deleteVehicle, registerKilometers } from '../controller/vehicle.controller.js';
+
+const vehicleRoutes = express.Router();
+
+vehicleRoutes.route('/')
+  .get(getPagedVehicles) // Paginated
+  .post(createVehicle); // Create
+
+vehicleRoutes.route('/:id')
+  .get(getVehicle)
+  .put(updateVehicle)
+  .delete(deleteVehicle);
+
+vehicleRoutes.route('/:id/status')
+  .get(checkVehicleStatus); // Paginated
+
+vehicleRoutes.route('/:id/fillTank')
+  .put(fillVehicleTank); // id, gasAmount
+
+vehicleRoutes.route('/:id/registerKilometers')
+  .put(registerKilometers); // id, kilometers
+
+
+export default vehicleRoutes;
