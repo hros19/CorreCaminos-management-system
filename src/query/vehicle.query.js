7c059935cc5f1d61a1bcf468c2b2c13d3bf8774f
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-11 19:29:41 -0500

    Vehicle Query implemented

---
+++ src/query/vehicle.query.js
@@ -0,0 +1,11 @@
+const VEHICLE_QUERY = {
+  CHECK_VEHICLE_STATUS: 'CALL get_veh_status(?)', // set
+  FILL_VEHICLE_TANK: 'CALL fillVehicleTank(?, ?)',
+  CREATE_VEHICLE: 'CALL create_vehicle(?, ?, ?, ?, ?, ?)', // sets
+  SELECT_PAGED_VEHICLES: 'CALL getp_vehicles(?, ?, ?, ?, ?)', // sets
+  SELECT_VEHICLE: 'SELECT * FROM Vehicle WHERE vehicle_id = ?', // set
+  UPDATE_VEHICLE: 'CALL upd_vehicle_details(?, ?, ?, ?, ?)',
+  DELETE_VEHICLE: 'DELETE FROM Vehicle WHERE vehicle_id = ?'
+}
+
+export default VEHICLE_QUERY;
