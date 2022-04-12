a8cf360c2dcbd4863c380ae0681c6e2860c32a7a
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-12 01:46:59 -0500

    Adding and modifying queries.
    Just in case I made 'GET_TOTAL_VEHICLES' query.

--- src/query/vehicle.query.js
+++ src/query/vehicle.query.js
@@ -1,8 +1,9 @@
 const VEHICLE_QUERY = {
+  GET_TOTAL_VEHICLES: 'SELECT COUNT(*) FROM Vehicle',
   CHECK_VEHICLE_STATUS: 'CALL get_veh_status(?)', // set
   FILL_VEHICLE_TANK: 'CALL fillVehicleTank(?, ?)',
   CREATE_VEHICLE: 'CALL create_vehicle(?, ?, ?, ?, ?, ?)', // sets
-  SELECT_PAGED_VEHICLES: 'CALL getp_vehicles(?, ?, ?, ?, ?)', // sets
+  SELECT_PAGED_VEHICLES: 'CALL getp_vehicles(?, ?, ?, ?)', // sets
   SELECT_VEHICLE: 'SELECT * FROM Vehicle WHERE vehicle_id = ?', // set
   SELECT_VEHICLES: 'SELECT * FROM Vehicle', // sets
   UPDATE_VEHICLE: 'CALL upd_vehicle_details(?, ?, ?, ?, ?)',
