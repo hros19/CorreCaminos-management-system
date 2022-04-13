ad90194a275dbe71e5e64f675cee948df922ae3f
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-13 00:48:59 -0500

    Create driver.query.js

---
+++ src/query/driver.query.js
@@ -0,0 +1,10 @@
+const DRIVER_QUERY = {
+  CREATE_DRIVER: 'CALL create_driver(?, ?, ?, ?, ?)',
+  UPDATE_DRIVER: 'CALL upd_driver(?, ?, ?, ?, ?, ?)',
+  SELECT_PAGED_DRIVERS: 'CALL getp_drivers(?, ?, ?, ?)',
+  SELECT_DRIVERS: 'SELECT * FROM Driver',
+  SELECT_DRIVER: 'SELECT * FROM Driver WHERE driver_id = ?',
+  DELETE_DRIVER: 'DELETE FROM Driver WHERE driver_id = ?'
+};
+
+export default DRIVER_QUERY;
