b2d9bbe08858e7e849de3fb219a49ce16fdb95da
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-13 16:10:25 -0500

    Adding extra routes

--- src/route/zone.route.js
+++ src/route/zone.route.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { createZone, getZone, getPagedZones, updateZone, deleteZone } from '../controller/zone.controler.js'; 
+import { createZone, getZone, getPagedZones, updateZone, deleteZone } from '../controller/zone.controler.js';
+import { createRoute, getRoute, getPagedRoutes, updateRoute, deleteRoute } from '../controller/zone.controller.js';
+import { createZoneXRoute, getPagedZoneRoutes, deleteZoneXRoute } from '../controller/zone.controller.js';
 
 const zoneRoutes = express.Router();
 
+//Properly zone routes
 zoneRoutes.route('/')
   .get(getPagedZones) // Paginated
   .post(createZone)
@@ -11,3 +14,23 @@ zoneRoutes.route('/:id')
   .get(getZone)
   .put(updateZone)
   .delete(deleteZone);
+
+//Route routes XD
+zoneRoutes.route('/route')
+  .get(getPagedRoutes) // Paginated
+  .post(createRoute);
+
+zoneRoutes.route('/route/:route_id')
+  .get(getRoute)
+  .put(updateRoute)
+  .delete(deleteRoute)
+
+zoneRoutes.route('/:id/route')
+  .get(getPagedZoneRoutes);
+
+//ZoneXRoute routes
+zoneRoutes.route('/:id/route/:route_id')
+  .post(createZoneXRoute)
+  .delete(deleteZoneXRoute);
+
+export default zoneRoutes;
