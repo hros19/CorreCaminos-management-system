00b8bf0a057fc104f1a388726ad54300cdfc8394
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-12 19:19:31 -0500

    Enhanced route
    New routes for vehicles.

--- src/route/vehicle.route.js
+++ src/route/vehicle.route.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import { checkVehicleStatus, fillVehicleTank, createVehicle, getPagedVehicles, getVehicle, updateVehicle, deleteVehicle, registerKilometers } from '../controller/vehicle.controller.js';
+import { checkVehicleStatus, fillVehicleTank, createVehicle, getPagedVehicles, getMaintenanceLogsOfVehicle, deleteMaintenanceLog, getMaintenanceLog } from '../controller/vehicle.controller.js';
+import { getVehicle, updateVehicle, deleteVehicle, registerKilometers, updateMaintenenaceLog, registerMaintenanceLog } from '../controller/vehicle.controller.js';
+
 
 const vehicleRoutes = express.Router();
 
@@ -20,5 +22,16 @@ vehicleRoutes.route('/:id/fillTank')
 
 vehicleRoutes.route('/:id/registerKilometers')
   .put(registerKilometers); // id, kilometers
+
+vehicleRoutes.route('/:id/registerMaintenance')
+  .post(registerMaintenanceLog); // status
+
+vehicleRoutes.route('/:id/maintenances')
+  .get(getMaintenanceLogsOfVehicle); // id, page, limit
+
+vehicleRoutes.route('/maintenance/:id')
+  .get(getMaintenanceLog)
+  .put(updateMaintenenaceLog) // maintenance_id, status
+  .delete(deleteMaintenanceLog);
 
 export default vehicleRoutes;
