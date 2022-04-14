7f4b108c59e401d7ce4b1fdb44fc706bfe4a89b4
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-13 19:07:07 -0600

    supplierRoutes added

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import vehicleRoutes from './route/vehicle.route.js';
 import jobtitleRoutes from './route/jobtitle.route.js';
 import driverRoutes from './route/driver.route.js';
 import businesstypeRoute from './route/businesstype.route.js';
+import supplierRoutes from './route/supplier.route.js';
 import zoneRoutes from './route/zone.route.js';
 
 dotenv.config();
@@ -24,6 +25,7 @@ app.use('/jobtitle', jobtitleRoutes);
 app.use('/driver', driverRoutes);
 app.use('/businesstype', businesstypeRoute);
 app.use('/zone', zoneRoutes);
+app.use('/supplier', supplierRoutes);
 app.all('*', (req, res) => {
   res.status(HttpStatus.NOT_FOUND.code)
     .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `The resource ${req.originalUrl} was not found`));
