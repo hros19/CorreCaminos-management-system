6a16541228a51e9da16630b5186942135a70a446
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-13 22:26:22 -0600

    SubCategory routes added

--- src/route/productcategory.route.js
+++ src/route/productcategory.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { createProductCategory, getPagedProductCategories, getProductCategory, updateProductCategory, deleteProductCategory } from '../controller/productcategory.controller';
+import { createProductSubCat, getProductSubCategory, getPagedProductSubCats, updateProductSubCat, deleteProductSubCat  } from '../controller/productcategory.controller.js';
 
 const productCategoryRoutes = express.Router();
 
+// Product Category routes
 productCategoryRoutes.route('/')
   .get(getPagedProductCategories) // Paginated
   .post(createProductCategory);
@@ -11,5 +13,15 @@ productCategoryRoutes.route('/:id')
   .get(getProductCategory)
   .put(updateProductCategory)
   .delete(deleteProductCategory);
+
+// Product Sub Category routes
+productCategoryRoutes.route('/subCategory')
+  .get(getPagedProductSubCats) // Paginated
+  .post(createProductSubCat);
+
+productCategoryRoutes.route('/subCategory/:id')
+  .get(getProductSubCategory)
+  .put(updateProductSubCat)
+  .delete(deleteProductSubCat);
 
 export default productCategoryRoutes;
