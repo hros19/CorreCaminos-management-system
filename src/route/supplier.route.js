bab2e162a842ed7314e3359cfcbd15558e92b227
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-16 17:02:05 -0500

    New routes pending to test

--- src/route/supplier.route.js
+++ src/route/supplier.route.js
@@ -1,17 +1,22 @@
 import express from 'express';
-import { createSupplier, updateSupplier, deleteSupplier, getPagedSuppliers, getSupplier, getProductsBySupplier } from '../controller/supplier.controller.js';
+import { createSupplier, updateSupplier, deleteSupplier, getPagedSuppliers, getSupplier, getProductsBySupplier, getOrdersBySupplier } from '../controller/supplier.controller.js';
 
 const supplierRoutes = express.Router();
 
 supplierRoutes.route('/')
-  .get(getPagedSuppliers)
-  .post(createSupplier);
+  .get(getPagedSuppliers) // parameter, order, pag, limit
+  .post(createSupplier); // name, address, phone, email, haveDelivery (YES/NO)
 
 supplierRoutes.route('/:id')
   .get(getSupplier)
   .put(updateSupplier)
   .delete(deleteSupplier);
+
 supplierRoutes.route('/:id/products')
   .get(getProductsBySupplier);
+
+supplierRoutes.route('/:id/orders')
+  .get(getOrdersBySupplier);
+
 
 export default supplierRoutes;
