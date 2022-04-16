49b7e2f243b7d8c1b142a88a311c7a73c3e03d31
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-16 17:01:02 -0500

    Added new routes (Pending to test)

--- src/route/business_stock.route.js
+++ src/route/business_stock.route.js
@@ -1,9 +1,18 @@
 import express from 'express';
-import { fillProductsInStock } from '../controller/business_stock.controller.js';
+import { getPagedProductsInStock, getProdInStock, registerProductInStock } from '../controller/business_stock.controller.js';
+import { fillProductsInStock, unregProductInStock, deleteProductInStock } from '../controller/business_stock.controller.js';
 
 const businessStockRoutes = express.Router();
 
-businessStockRoutes.route('/fillProducts')
-  .get(fillProductsInStock)
+businessStockRoutes.route('/')
+  .get(getPagedProductsInStock) // Paginated
+  .post(fillProductsInStock); // { product_id, amount }
+
+businessStockRoutes.route('/:id')
+  .get(getProdInStock)
+  .delete(deleteProductInStock)
+  .put(unregProductInStock)
+  .post(registerProductInStock); // { product_id, amount }
+
 
 export default businessStockRoutes;
