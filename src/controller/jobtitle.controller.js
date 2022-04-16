95d87a967bfa1fad245061cfdc38a5d09a756f5a
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-16 16:59:03 -0500

    Added logs and fixing logic

--- src/controller/jobtitle.controller.js
+++ src/controller/jobtitle.controller.js
@@ -13,12 +13,14 @@ export const createJobTitle = (req, res) => {
   const BODY_PARAMETERS = Object.values(req.body);
   // Checking quantity of parameters
   if (BODY_PARAMETERS.length != 1) {
+    logger.error(`${req.method} ${req.originalUrl}, invalid parameters quantity`);
     res.status(HttpStatus.BAD_REQUEST.code)
       .send(new Response(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `Invalid number of parameters`));
     return;
   }
   // Checking no empty parameters
   if (BODY_PARAMETERS.includes('', "")) {
+    logger.error(`${req.method} ${req.originalUrl}, empty parameters`);
     res.status(HttpStatus.BAD_REQUEST.code)
       .send(new Response(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `Empty parameters not allowed`));
     return;
@@ -26,22 +28,27 @@ export const createJobTitle = (req, res) => {
   database.query(JOBTITLE_QUERY.CREATE_JOBTITLE, BODY_PARAMETERS, (error, results) => {
     if (error) {
       if (error.errno == 1064) {
+        logger.error(`${req.method} ${req.originalUrl}, ${error.code} - ${error.errno} - ${error.sqlMessage}`);
         res.status(HttpStatus.BAD_REQUEST.code)
           .send(new Response(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `Invalid parameters values, check documentation`));
         return;
       }
       if (error.errno == 1062) {
+        logger.error(`${req.method} ${req.originalUrl}, job title already exists`);
         res.status(HttpStatus.BAD_REQUEST.code)
           .send(new Response(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `Duplicated values, the object was not created`));
         return;
       }
+      logger.error(`${req.method} ${req.originalUrl}, error creating job title: ${error}`);
       res.status(HttpStatus.INTERNAL_SERVER_ERROR.code)
         .send(new Response(HttpStatus.INTERNAL_SERVER_ERROR.code, HttpStatus.INTERNAL_SERVER_ERROR.status, `Unexpected behavior`));
     } else {
       if (!results) {
+        logger.error(`${req.method} ${req.originalUrl}, error creating job title`);
         res.status(HttpStatus.BAD_REQUEST.code)
           .send(new Response(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `The job title was not created`));
       } else {
+        logger.info(`${req.method} ${req.originalUrl}, job title created`);
         const jobtitle = results[0][0];
         res.status(HttpStatus.CREATED.code)
           .send(new Response(HttpStatus.CREATED.code, HttpStatus.CREATED.status, `Job title registered successfully`, { jobtitle }));
@@ -52,8 +59,9 @@ export const createJobTitle = (req, res) => {
 
 export const getPagedJobTitles = (req, res) => {
   logger.info(`${req.method} ${req.originalUrl}, retrieving job titles...`);
-  const order = req.param('order');
+  const order = req.body.order || 'ASC';
   if (!ORDER_VALUES.includes(order)) {
+    logger.error(`${req.method} ${req.originalUrl}, invalid order value`);
     res.status(HttpStatus.BAD_REQUEST.code)
       .send(new Response(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `Check 'order' values`));
     return;
@@ -63,8 +71,8 @@ export const getPagedJobTitles = (req, res) => {
       res.status(HttpStatus.NOT_FOUND.code)
         .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `Job titles not found`));
     } else {
-      const page = req.param('pag') ? Number(req.param('pag')) : 1;
-      const limit = req.param('limit') ? Number(req.param('limit')) : 1;
+      const page = Number(req.body.pag) || 1;
+      const limit = Number(req.body.limit) || 10;
       // Validation of pagination parameters
       if (isNaN(page) || isNaN(limit)) {
         res.status(HttpStatus.BAD_REQUEST.code)
@@ -75,11 +83,13 @@ export const getPagedJobTitles = (req, res) => {
       let numOfResults = results.length;
       let numOfPages = Math.ceil(numOfResults / limit);
       if (page > numOfPages) {
+        logger.error(`${req.method} ${req.originalUrl}, page ${page} not found`);
         res.status(HttpStatus.BAD_REQUEST.code)
           .send(new Response(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `Selected page exceed total page number. The total pages is ${numOfPages} and the page ${page} was requested`));
         return;
       }
       if (page < 1) {
+        logger.error(`${req.method} ${req.originalUrl}, page number must be greater than 0`);
         res.status(HttpStatus.BAD_REQUEST.code)
           .send(new Response(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `Invalid page requested (${page}), must be 1 or higher`));
         return;
@@ -88,17 +98,19 @@ export const getPagedJobTitles = (req, res) => {
       const startingLimit = (page - 1) * limit;
       database.query(JOBTITLE_QUERY.SELECT_PAGED_JOBTITLES, [order, startingLimit, limit], (error, results) => {
         if (error) {
-          console.log(error);
+          logger.error(`${req.method} ${req.originalUrl}, error retrieving job titles: ${error}`);
           res.status(HttpStatus.INTERNAL_SERVER_ERROR.code)
             .send(new Response(HttpStatus.INTERNAL_SERVER_ERROR.code, HttpStatus.INTERNAL_SERVER_ERROR.status, `Unexpected behavior`));
           return;
         } else {
           if (!results[0]) {
+            logger.error(`${req.method} ${req.originalUrl}, error retrieving job titles: ${error}`);
             res.status(HttpStatus.NOT_FOUND.code)
               .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `Not job titles found`));
           } else {
+            logger.info(`${req.method} ${req.originalUrl}, job titles retrieved successfully`);
             res.status(HttpStatus.OK.code)
-              .send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, { data: results[0], page, numOfPages }));
+              .send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Job title retrieved`, { data: results[0], page, numOfPages }));
           }
         }
       });
@@ -111,38 +123,44 @@ export const updateJobTitle = (req, res) => {
   const BODY_PARAMETERS = Object.values(req.body);
   // Checking quantity of parameters
   if (BODY_PARAMETERS.length != 1) {
+    logger.error(`${req.method} ${req.originalUrl}, invalid number of parameters`);
     res.status(HttpStatus.BAD_REQUEST.code)
       .send(new Response(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `Invalid number of parameters`));
     return;
   }
   // Checking no-empty parameters
   if (BODY_PARAMETERS.includes('', "")) {
+    logger.error(`${req.method} ${req.originalUrl}, empty parameters not allowed`);
     res.status(HttpStatus.BAD_REQUEST.code)
       .send(new Response(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `Invalid values for pagination (check 'pag' and 'limit' values in the request)`));
     return;
   }
   database.query(JOBTITLE_QUERY.SELECT_JOBTITLE, [req.params.id], (error, results) => {
     if (!results[0]) {
+      logger.error(`${req.method} ${req.originalUrl}, job title not found`);
       res.status(HttpStatus.NOT_FOUND.code)
         .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `The job title with id ${req.params.id} was not found`));
       return;
     }
-    database.query(JOBTITLE_QUERY.UPDATE_JOBTITLE, [req.params.id, ...BODY_PARAMETERS], (error, results) => {
+    database.query(JOBTITLE_QUERY.UPDATE_JOBTITLE, [req.params.id, BODY_PARAMETERS[0]], (error, results) => {
       if (error) {
         if (error.errno == 1064) {
+          logger.error(`${req.method} ${req.originalUrl}, invalid parameter values`);
           res.status(HttpStatus.BAD_REQUEST.code)
             .send(new Response(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `Invalid parameters value`));
           return;
         }
         if (error.errno == 1062) {
+          logger.error(`${req.method} ${req.originalUrl}, duplicate entry`);
           res.status(HttpStatus.BAD_REQUEST.code)
             .send(new Response(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `Duplicated value, the object was not modified`));
           return;
         }
-        console.log(error);
+        logger.error(`${req.method} ${req.originalUrl}, unexpected behavior`);
         res.status(HttpStatus.INTERNAL_SERVER_ERROR.code)
           .send(new Response(HttpStatus.INTERNAL_SERVER_ERROR.code, HttpStatus.INTERNAL_SERVER_ERROR.status, `Unexpected behavior`));
       } else {
+        logger.info(`${req.method} ${req.originalUrl}, job title updated`);
         res.status(HttpStatus.OK.code)
           .send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Job title modified sucessfully`, { id: req.params.id, ...req.body }));
         return;
