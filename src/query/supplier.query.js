acb0cb95b780a20c91748851ad25c7213c7b1992
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-16 17:00:36 -0500

    Fixing miss-spelling

--- src/query/supplier.query.js
+++ src/query/supplier.query.js
@@ -1,8 +1,8 @@
 const SUPPLIER_QUERY = {
   CREATE_SUPPLIER: 'CALL create_supplier(?, ?, ?, ?, ?)',
   UPDATE_SUPPLIER: 'CALL upd_supplier(?, ?, ?, ?, ?, ?)',
-  DELETE_SUPPLIER: 'DELETE FROM Supplier WHERE id = ?',
-  SELECT_SUPPLIER: 'SELECT * FROM Supplier WHERE id = ?',
+  DELETE_SUPPLIER: 'DELETE FROM Supplier WHERE supplier_id = ?',
+  SELECT_SUPPLIER: 'SELECT * FROM Supplier WHERE supplier_id = ?',
   SELECT_SUPPLIERS: 'SELECT * FROM Supplier',
   SELECT_PAGED_SUPPLIERS: 'CALL getp_suppliers(?, ?, ?, ?)',
   GET_PAGED_PRODUCTS_BY_SUPPLIER: 'CALL getp_sup_products(?, ?, ?, ?, ?)',
