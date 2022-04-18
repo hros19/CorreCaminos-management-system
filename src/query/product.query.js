e25e878683db0c282f1405715c8b4913363dd53c
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-04-18 02:05:52 -0500

    Add extra parameter for price

--- src/query/product.query.js
+++ src/query/product.query.js
@@ -1,9 +1,9 @@
 const PRODUCT_QUERY = {
-  CREATE_PRODUCT: 'CALL create_product(?, ?, ?, ?)',
+  CREATE_PRODUCT: 'CALL create_product(?, ?, ?, ?, ?)', // c
   SELECT_PRODUCT: 'SELECT * FROM Product WHERE product_id = ?',
   SELECT_PRODUCTS: 'SELECT * FROM Product',
   SELECT_PAGED_PRODUCTS: 'CALL getp_products(?, ?, ?, ?)',
-  UPDATE_PRODUCT: 'CALL upd_product(?, ?, ?, ?)',
+  UPDATE_PRODUCT: 'CALL upd_product(?, ?, ?, ?, ?)', // c
   DELETE_PRODUCT: 'DELETE FROM Product WHERE product_id = ?'
 };
 
